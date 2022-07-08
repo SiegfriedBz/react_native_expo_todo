b2cab5ab8ff9160b58d387dcc9504615ef981afe
Siegfried <siegfried.bozza@renuo.ch>
2022-07-08 13:55:23 +0200

    added stack nav

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import { uuid } from 'uuidv4';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from 'react-navigation-stack';
+import { StyleSheet, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import TodoListScreen from './TodoListScreen';
 import NewTodoScreen from './NewTodoScreen';
@@ -35,22 +35,42 @@ export default function App() {
   }
 
   const styles = StyleSheet.create({
+    header: {
+      flex: 1,
+      backgroundColor: 'white',
+      justifyContent: 'center',
+    },
     container: {
       flex: 1,
       backgroundColor: 'white',
       alignItems: 'center',
       justifyContent: 'center',
     }
+    
   });
 
+  const Stack = createNativeStackNavigator();
+
   return (
+    <NavigationContainer>
         <TodoContext.Provider value={TodoContextValues}>
-          <View style={styles.container}>
-            <TodoListScreen />
-            <NewTodoScreen />
+          <View 
+            style={styles.header}
+            >
+            <Stack.Navigator>
+              <Stack.Screen 
+                name='TodoList'
+                component={TodoListScreen}
+              />
+              <Stack.Screen 
+                name='NewTodo'
+                component={NewTodoScreen}
+              />
+            </Stack.Navigator>
           <StatusBar style="auto" />
           </View>
         </TodoContext.Provider>
+    </NavigationContainer>
   );
 }
 
